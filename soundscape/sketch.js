07ab91d2f0cbc61b8bebbf42710a6a7c18aa0d4a
ilzite <emsinailze@gmail.com>
2024-04-15 02:22:03 +0300

    Update sketch.js

--- soundscape/sketch.js
+++ soundscape/sketch.js
@@ -3,10 +3,10 @@ let hasUserClicked = false;
 let circleX = 300;
 let circleY = 400;
 
-//let nightSound;
+let nightSound;
 let retroSound;
 function preload() {
- // nightSound = loadSound('assets/night.mp3');
+nightSound = loadSound('assets/night.mp3');
   retroSound = loadSound('assets/retro.mp3');
 }
 
@@ -19,34 +19,46 @@ function setup() {
 function draw() {
 
   background('lightgreen');
-  circle(circleX, circleY, 50);
+  // circle(circleX, circleY, 50);
 
   if (hasUserClicked == false) {
     text('hi, please click to start', width / 2, height / 2);
   } else {
+    if (nightSound.isPlaying() == false) {
+      nightSound.play()
+    }
+
+    let calculatedVolumeNight = map(
+      mouseX,           // the changing number
+      0,                //min expected number
+      width,            //max expected number
+      0,                //min output number
+      1                 //max output number
+     );
+
     if (retroSound.isPlaying() == false) {
-        retroSound.play()
+        retroSound.play();
     }
 
     let distanceToCircle = dist(
       mouseX, mouseY,
       circleX, circleY
-    )
+    );
     let calculateVolumeRetro = map(
      distanceToCircle,  //the changing number
      0,                 //min expected number
      200,               //max expected number
      1,                 //min output number
      0                  //max output number
-    )
+    );
 
     calculateVolumeRetro = constrain(
       calculateVolumeRetro, 0, 1
-    )
+    );
 
-    //volume needs a number beteen 0 - 1
-   // nightSound.setVolume(calculateVolumeNight, 2);
-    retroSound.setVolume(calculateVolumeRetro, 2);
+    //volume needs a number between 0 - 1
+    nightSound.setVolume(calculateVolumeNight);
+    retroSound.setVolume(calculateVolumeRetro);
   }
 }
 
